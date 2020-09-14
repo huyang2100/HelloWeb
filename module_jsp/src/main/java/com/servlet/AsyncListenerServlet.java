372e5b47cbd55457fcfb12c457c92b9f3757607f
huyang <972603060@qq.com>
2020-09-14 14:20:52 +0800

    注册AsyncListener监听异步任务的完成状态

---
+++ module_jsp/src/main/java/com/servlet/AsyncListenerServlet.java
@@ -0,0 +1,37 @@
+package com.servlet;
+
+import com.listener.MyAsyncListener;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/asyncListener",asyncSupported = true)
+public class AsyncListenerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        AsyncContext asyncContext = req.startAsync();
+        asyncContext.setTimeout(5000);
+        // register an listener
+        asyncContext.addListener(new MyAsyncListener());
+        asyncContext.start(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                String greeting = "hi from listener";
+                System.out.println("wait...");
+                req.setAttribute("greeting",greeting);
+                asyncContext.dispatch("/test.jsp");
+            }
+        });
+    }
+}
---
+++ module_jsp/src/main/webapp/test.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: huyang
+  Date: 2020/9/14
+  Time: 2:16 下午
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+${requestScope.greeting}
+</body>
+</html>
