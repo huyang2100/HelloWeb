2edb096bc4b4714621684293bd91cf2c95775733
huyang <972603060@qq.com>
2020-09-16 16:11:18 +0800

    servlet容器加载

---
+++ module_jsp/src/main/java/com/initializer/MyServletContainerInitializer.java
@@ -0,0 +1,17 @@
+package com.initializer;
+
+import com.servlet.FirstServlet;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.HandlesTypes;
+import java.util.Set;
+
+@HandlesTypes(FirstServlet.class)
+public class MyServletContainerInitializer implements ServletContainerInitializer {
+    @Override
+    public void onStartup(Set<Class<?>> set, ServletContext servletContext) throws ServletException {
+
+    }
+}
